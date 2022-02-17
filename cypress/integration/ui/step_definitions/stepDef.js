8f38950677f43de570f751ab1f89d37159affc6a
Ranjit005 <ranjit.barik@batesville.com>
2022-02-17 07:23:00 +0530

    Changes in Step Def File

--- cypress/integration/ui/step_definitions/stepDef.js
+++ cypress/integration/ui/step_definitions/stepDef.js
@@ -3,6 +3,7 @@ Given('I navigate to Google Page', () => {
   cy.visit("https://www.google.co.in/")
 });
 Given('I click on Search Link', () => {
-    cy.xpath("//a[contains(text(),'Sign in')]").click()
+  cy.log("Reaching tO Search Page")
+    //cy.xpath("//a[contains(text(),'Sign in')]").click()
     //cy.xpath("//a[contains(text(),'NonSense')]").should('be.visible')
   });
