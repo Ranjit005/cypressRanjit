794fdb56d273d9c81705baec879345e66cc5472a
Ranjit005 <ranjit.barik@batesville.com>
2022-02-17 07:27:34 +0530

    Fail test case in step def

--- cypress/integration/ui/step_definitions/stepDef.js
+++ cypress/integration/ui/step_definitions/stepDef.js
@@ -5,5 +5,5 @@ Given('I navigate to Google Page', () => {
 Given('I click on Search Link', () => {
   cy.log("Reaching tO Search Page")
     //cy.xpath("//a[contains(text(),'Sign in')]").click()
-    //cy.xpath("//a[contains(text(),'NonSense')]").should('be.visible')
+  cy.xpath("//a[contains(text(),'NonSense')]").should('be.visible')
   });
